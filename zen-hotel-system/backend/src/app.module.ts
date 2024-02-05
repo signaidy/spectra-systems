9fd960a6ef50665d63826e5627a7458976ca280a
Max <111196993+NqMax@users.noreply.github.com>
2024-02-05 01:48:51 -0600

    Removed files not scoped to the individual hotel chain

--- zen-hotel-system/backend/src/app.module.ts
+++
@@ -1,6 +0,0 @@
-import { Module } from "@nestjs/common";
-import { MarriotModule } from "./marriot/marriot.module";
-@Module({
-  imports: [MarriotModule],
-})
-export class AppModule {}
--- zen-hotel-system/backend/src/marriot/marriot.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from "@nestjs/common";
-import { MarriotController } from "./marriot.controller";
-import { MarriotService } from "./marriot.service";
-
-@Module({
-  controllers: [MarriotController],
-  providers: [MarriotService],
-})
-export class MarriotModule {}
