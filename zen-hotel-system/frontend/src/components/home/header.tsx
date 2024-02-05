9e65e03f434a04c22d39ea042ca2839ba3d35cea
Max <111196993+NqMax@users.noreply.github.com>
2024-02-05 01:33:03 -0600

    Added link to header logo

--- zen-hotel-system/frontend/src/components/home/header.tsx
+++ zen-hotel-system/frontend/src/components/home/header.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 import { HeaderNav } from "@/components/home/headerNav";
 import { SignInButton } from "@/components/home/signInButton";
 
@@ -6,13 +7,15 @@ export function Header() {
   return (
     <header className="h-20 border-b shadow">
       <div className="container flex justify-between items-center h-full">
-        <Image
-          src={`/zen-logo.png`}
-          alt="Zen Hotels Logo"
-          width="150"
-          height="60"
-          priority
-        />
+        <Link href="/">
+          <Image
+            src={`/zen-logo.png`}
+            alt="Zen Hotels Logo"
+            width="150"
+            height="60"
+            priority
+          />
+        </Link>
         <HeaderNav />
         <SignInButton />
       </div>
