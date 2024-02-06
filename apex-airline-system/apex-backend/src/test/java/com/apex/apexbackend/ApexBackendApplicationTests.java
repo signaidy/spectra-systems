39300c2bdf2c54a5bc177a9a199bad00f2dec144
Max <111196993+NqMax@users.noreply.github.com>
2024-02-05 23:59:03 -0600

    Initial airline backend commit using Spring

---
+++ apex-airline-system/apex-backend/src/test/java/com/apex/apexbackend/ApexBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apex.apexbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApexBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
