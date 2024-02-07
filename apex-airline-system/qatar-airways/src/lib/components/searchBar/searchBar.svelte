afb880becf018173261c9d1562dfbceafd0d285c
Max <111196993+NqMax@users.noreply.github.com>
2024-02-06 23:08:31 -0600

    Implemented search bar with picker components

---
+++ apex-airline-system/qatar-airways/src/lib/components/searchBar/searchBar.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+  import LocationPicker from "$lib/components/searchBar/locationPicker.svelte";
+  import DateRangePicker from "$lib/components/searchBar/dateRangePicker.svelte";
+  import NumberPicker from "$lib/components/searchBar/numberPicker.svelte";
+  import { Button } from "$lib/components/ui/button";
+</script>
+
+<div class="bg-white rounded-md p-3 shadow border flex gap-x-3 flex-wrap gap-y-3">
+  <div class="flex flex-col gap-y-2">
+    <div class="font-bold">From?</div>
+    <LocationPicker />
+  </div>
+  <div class="flex flex-col gap-y-2">
+    <div class="font-bold">To?</div>
+    <LocationPicker />
+  </div>
+  <div class="flex flex-col gap-y-2">
+    <div class="font-bold">When?</div>
+    <DateRangePicker />
+  </div>
+  <div class="flex flex-col gap-y-2">
+    <div class="font-bold">How Many?</div>
+    <NumberPicker />
+  </div>
+  <div class="flex flex-col gap-y-2">
+    <div class="text-background">Find</div>
+    <Button>
+      Find Flights
+    </Button>
+  </div>
+</div>
