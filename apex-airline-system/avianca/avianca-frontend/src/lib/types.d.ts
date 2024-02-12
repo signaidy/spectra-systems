e32cbcfece9c34b925f97894e246b89f6b29d848
Max <111196993+NqMax@users.noreply.github.com>
2024-02-12 00:11:17 -0600

    Created types definition file

---
+++ apex-airline-system/avianca/avianca-frontend/src/lib/types.d.ts
@@ -0,0 +1,11 @@
+interface User {
+  userId: string;
+  email: string;
+  firstName: string;
+  iat: number;
+  lastName: string;
+  originCountry: string;
+  passportNumber: string;
+  role: "user" | "admin" | "enterprise";
+  age: string;
+}
