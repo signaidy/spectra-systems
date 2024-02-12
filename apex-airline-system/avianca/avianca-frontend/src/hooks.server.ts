005f523cd77e19adc2922d20eab3e776804fd67d
Max <111196993+NqMax@users.noreply.github.com>
2024-02-12 00:10:38 -0600

    Implemented authorization hook

---
+++ apex-airline-system/avianca/avianca-frontend/src/hooks.server.ts
@@ -0,0 +1,20 @@
+import { authenticateToken } from "$lib/server/auth";
+import { redirect, type Handle } from "@sveltejs/kit";
+
+export const handle: Handle = async ({ event, resolve }) => {
+  console.log("handle")
+  // Stage 1
+  event.locals.user = authenticateToken(event);
+
+  if (event.url.pathname.startsWith("/user")) {
+    if (!event.locals.user) {
+      throw redirect(303, "/login");
+    }
+  }
+
+  // Stage 2
+  const response = await resolve(event);
+
+  //Stage 3
+  return response;
+};
