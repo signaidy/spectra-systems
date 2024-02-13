9ee5ed8f9450ed92874a70a7940a2705b669e044
Max <111196993+NqMax@users.noreply.github.com>
2024-02-13 03:54:16 -0600

    Implemented flight creator UI

---
+++ apex-airline-system/avianca/avianca-frontend/src/lib/components/user/administration/flightCreator/flightCreator.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  import FlightTypePicker from "$lib/components/user/administration/flightCreator/flightTypePicker.svelte";
+  import OneWayFlight from "$lib/components/user/administration/flightCreator/oneWayFlight.svelte";
+</script>
+
+<div class="flex flex-col rounded-lg border">
+  <FlightTypePicker />
+  <OneWayFlight />
+</div>
---
+++ apex-airline-system/avianca/avianca-frontend/src/lib/components/user/administration/flightCreator/flightTypePicker.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  import { Label } from "$lib/components/ui/label";
+  import * as RadioGroup from "$lib/components/ui/radio-group";
+</script>
+
+<div class="p-3">
+  <div class="p-3 bg-background w-fit rounded-full">
+    <RadioGroup.Root value="round-trip" class="flex gap-x-3">
+      <div class="flex items-center gap-x-2">
+        <RadioGroup.Item value="one-way" id="one-way" />
+        <Label for="one-way">One Way</Label>
+      </div>
+      <div class="flex items-center gap-x-2">
+        <RadioGroup.Item value="round-trip" id="round-trip" />
+        <Label for="round-trip">Round Trip</Label>
+      </div>
+    </RadioGroup.Root>
+  </div>
+</div>
