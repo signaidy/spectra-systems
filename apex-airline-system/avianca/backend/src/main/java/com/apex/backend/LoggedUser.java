b728179467c8bf27d6b2e892992cba6cfa9bd5f3
Max <111196993+NqMax@users.noreply.github.com>
2024-02-20 02:20:45 -0600

    Added fields to logged user

--- apex-airline-system/avianca/backend/src/main/java/com/apex/backend/LoggedUser.java
+++ apex-airline-system/avianca/backend/src/main/java/com/apex/backend/LoggedUser.java
@@ -1,5 +1,5 @@
 package com.apex.backend;
 
 public record LoggedUser(String userId, String email, String firstName, String lastName, String originCountry,
-        String passportNumber, String role, String age) {
+        String passportNumber, String role, String age, String percentage, String entryDate) {
 }
