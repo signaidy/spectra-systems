56e54a9604a5f399e9c2e16cfa5f2b3c66344b3d
Sir_Anon <juanpablo08082002@hotmail.com>
2024-02-21 22:13:15 -0600

    Implementation of black arrow - User administration

--- apex-airline-system/avianca/avianca-frontend/src/routes/user/administration/users/+page.svelte
+++ apex-airline-system/avianca/avianca-frontend/src/routes/user/administration/users/+page.svelte
@@ -1,11 +1,13 @@
 <script lang="ts">
   import UserCard from "$lib/components/user/users/UserCard.svelte";
+  import {MoveLeft} from 'lucide-svelte';
 
   export let data;
   export let form;
 </script>
 
-<h1 class="text-xl font-bold mb-8">Users Administration</h1>
+<a href="/user/administration"><MoveLeft/></a>
+<h1 class="text-xl font-bold mt-4 mb-8">Users Administration</h1>
 <div class="flex flex-col gap-y-3">
   {#each data.users as user}
     <UserCard {form} user={user} />
