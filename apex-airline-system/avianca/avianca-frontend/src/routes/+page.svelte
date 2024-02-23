7fcaf29228d577b344c71a1e44a77cef0fb19f3e
Max <111196993+NqMax@users.noreply.github.com>
2024-02-23 13:33:51 -0600

    Added cities prop to search bar

--- apex-airline-system/avianca/avianca-frontend/src/routes/+page.svelte
+++ apex-airline-system/avianca/avianca-frontend/src/routes/+page.svelte
@@ -2,13 +2,15 @@
   import SearchBar from "$lib/components/searchBar/searchBar.svelte";
   import FeaturedFlights from "$lib/components/home/featuredFlights.svelte";
   import Footer from "$lib/components/footer/footer.svelte";
+
+  export let data;
 </script>
 
 <div
   class="h-[calc(100vh-5rem)] bg-[url('$lib/assets/home-background.jpg')] bg-cover"
 >
   <div class="flex flex-col container justify-center items-center h-full">
-    <SearchBar />
+    <SearchBar cities={data.cities}/>
   </div>
 </div>
 <FeaturedFlights />
