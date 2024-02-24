ab9c59cc0a11b77cd6555abb47e6bbb698aad752
Max <111196993+NqMax@users.noreply.github.com>
2024-02-24 07:27:50 -0600

    Commentaries and ratings implementation

--- apex-airline-system/avianca/avianca-frontend/src/routes/search/+page.server.ts
+++ apex-airline-system/avianca/avianca-frontend/src/routes/search/+page.server.ts
@@ -1,3 +1,5 @@
+import { fail } from "@sveltejs/kit";
+
 export function load({ locals, url }) {
   async function getOneWayFlights() {
     const response = await fetch(
@@ -8,6 +10,37 @@ export function load({ locals, url }) {
     );
 
     const result = await response.json();
+
+    function assignChildren(commentaries: Commentary[]): Commentary[] {
+      const commentariesById: { [key: number]: Commentary } = {};
+
+      commentaries.forEach((commentary) => {
+        commentariesById[commentary.commentId] = commentary;
+      });
+
+      commentaries.forEach((commentary) => {
+        const { parentCommentId } = commentary;
+        if (parentCommentId !== 0) {
+          const parentCommentary = commentariesById[parentCommentId];
+          if (parentCommentary) {
+            if (!parentCommentary.children) {
+              parentCommentary.children = [];
+            }
+            parentCommentary.children.push(commentary);
+          }
+        }
+      });
+
+      return commentaries.filter(
+        (commentary) => commentary.parentCommentId === 0
+      );
+    }
+
+    result.forEach((flight: Flight) => {
+      const arrangedCommentaries = assignChildren(flight.commentaries);
+      flight.commentaries = arrangedCommentaries;
+    });
+
     return result;
   }
 
@@ -26,3 +59,61 @@ export function load({ locals, url }) {
     flights: getOneWayFlights(),
   };
 }
+
+export const actions = {
+  createCommentary: async ({ request }) => {
+    const data = await request.formData();
+    try {
+      const response = await fetch("http://localhost:8080/create-commentary", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          parentId: data.get("parentId"),
+          userId: data.get("userId"),
+          content: data.get("content"),
+          flightId: data.get("flightId"),
+        }),
+      });
+      const result = await response.json();
+
+      if (result.error) {
+        throw new Error(result.error);
+      }
+    } catch (error) {
+      if (error instanceof Error) {
+        return fail(500, {
+          error: error.message,
+        });
+      }
+    }
+  },
+  createRating: async ({ request }) => {
+    const data = await request.formData();
+    try {
+      const response = await fetch("http://localhost:8080/create-rating", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          userId: data.get("userId"),
+          flightId: data.get("flightId"),
+          value: data.get("rating"),
+        }),
+      });
+      const result = await response.json();
+
+      if (result.error) {
+        throw new Error(result.error);
+      }
+    } catch (error) {
+      if (error instanceof Error) {
+        return fail(500, {
+          error: error.message,
+        });
+      }
+    }
+  },
+};
