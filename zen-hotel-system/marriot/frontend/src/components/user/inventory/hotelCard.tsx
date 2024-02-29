aad1a98f6bcf60b605ff1ab9b26d00a794f628d1
Max <111196993+NqMax@users.noreply.github.com>
2024-02-28 22:25:47 -0600

    Hotel card UI changes

--- zen-hotel-system/marriot/frontend/src/components/user/inventory/hotelCard.tsx
+++ zen-hotel-system/marriot/frontend/src/components/user/inventory/hotelCard.tsx
@@ -1,6 +1,9 @@
 import Image from "next/image";
+// Components
+import { HotelDialog } from "@/components/hotels/hotelDialog";
+// UI Components
 import { Button } from "@/components/ui/button";
-import { Star, MapPin, ArrowRight } from "lucide-react";
+import { Star, MapPin } from "lucide-react";
 
 export function HotelCard({
   name,
@@ -26,26 +29,21 @@ export function HotelCard({
           className="object-cover rounded-l-md"
         />
       </div>
-      <div className="flex flex-col justify-between rounded-r-md">
-        <div className="flex flex-col gap-y-1 p-4">
-          <h2 className="font-medium tracking-tight text-2xl">{name}</h2>
-          <div className="flex gap-x-2 text-sm items-center text-muted-foreground">
+      <div className="flex flex-col justify-between rounded-r-md bg-background">
+        <div className="flex flex-col gap-y-1 p-4 h-full">
+          <div className="font-medium tracking-tight text-xl">{name}</div>
+          <div className="flex gap-x-1 text-sm items-center text-muted-foreground">
             <div>{reviewAverage}</div>
-            <Star className="w-4 h-4 text-yellow-500" />
+            <Star className="w-4 h-4 text-black fill-yellow-500" />
             <div>({reviewCount} reviews)</div>
             <div>|</div>
-            <MapPin className="w-4 h-4 text-red-700" />
+            <MapPin className="w-4 h-4 text-black fill-red-700" />
             <div>{location}</div>
           </div>
-          <div className="flex flex-col gap-y-3 mt-5">
-            <p className="text-sm line-clamp-2">{description}</p>
-            <Button variant="link" className="w-fit p-0">
-              View Hotel Details
-              <ArrowRight className="ml-1 w-4 h-4" />
-            </Button>
-          </div>
+          <p className="text-sm line-clamp-3 mt-4">{description}</p>
+          <HotelDialog />
         </div>
-        <div className="border-t p-4 flex justify-end gap-x-3">
+        <div className="border-t p-3 flex justify-end gap-x-3">
           <div className="flex gap-x-1 items-end">
             <div className="font-bold text-xl">255</div>
             <div className="text-sm mb-[2px] text-muted-foreground">
