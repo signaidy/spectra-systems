d81da6e7d120464b65f4fa4d89bb4ec5189e7a6c
Max <111196993+NqMax@users.noreply.github.com>
2024-02-28 22:21:41 -0600

    Renamed component  and style changes

--- zen-hotel-system/marriot/frontend/src/components/user/navBar.tsx
+++ zen-hotel-system/marriot/frontend/src/components/user/navBar.tsx
@@ -1,7 +1,9 @@
 "use client";
 import { usePathname } from "next/navigation";
+// Components
 import { Button } from "@/components/ui/button";
 import { NavButton } from "@/components/user/navButton";
+// Icons
 import {
   LayoutDashboard,
   WalletCards,
@@ -17,44 +19,44 @@ const routes = [
     value: "dashboard",
     label: "Dashboard",
     href: "/dashboard",
-    icon: <LayoutDashboard className="w-4 h-4 mr-3" />,
+    icon: <LayoutDashboard className="w-4 h-4 mr-3 shrink-0" />,
   },
   {
     value: "reservations",
     label: "Reservations",
     href: "/reservations",
-    icon: <BedDouble className="w-4 h-4 mr-3" />,
+    icon: <BedDouble className="w-4 h-4 mr-3 shrink-0" />,
   },
   {
     value: "inventory",
     label: "Inventory",
     href: "/inventory",
-    icon: <Table2 className="w-4 h-4 mr-3" />,
+    icon: <Table2 className="w-4 h-4 mr-3 shrink-0" />,
   },
   {
     value: "administration",
     label: "Administration",
     href: "/administration",
-    icon: <Bolt className="w-4 h-4 mr-3" />,
+    icon: <Bolt className="w-4 h-4 mr-3 shrink-0" />,
   },
   {
     value: "purchase-logs",
     label: "Purchase Logs",
     href: "/purchase-logs",
-    icon: <WalletCards className="w-4 h-4 mr-3" />,
+    icon: <WalletCards className="w-4 h-4 mr-3 shrink-0" />,
   },
   {
     value: "analytics",
     label: "Analytics",
     href: "/analytics",
-    icon: <AreaChart className="w-4 h-4 mr-3" />,
+    icon: <AreaChart className="w-4 h-4 mr-3 shrink-0" />,
   },
 ];
 
-export function Nav() {
+export function NavBar() {
   const currentPath = usePathname();
   return (
-    <nav className="flex flex-col py-8 pr-8 border-r gap-y-3 sticky top-0">
+    <nav className="flex flex-col py-8 pr-8 border-r gap-y-3 shrink-0 overflow-y-auto">
       {routes.map((route) => (
         <NavButton
           variant={currentPath.includes(route.value) ? "default" : "secondary"}
@@ -65,7 +67,10 @@ export function Nav() {
           {route.label}
         </NavButton>
       ))}
-      <Button>Log Out</Button>
+      <Button className="justify-normal">
+        <LogOut className="w-4 h-4 mr-3 shrink-0" />
+        Log Out
+      </Button>
     </nav>
   );
 }
