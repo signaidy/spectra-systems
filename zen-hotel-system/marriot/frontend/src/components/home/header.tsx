1bf2eaa18a622a9b556a37e9872a465853a3c669
Max <111196993+NqMax@users.noreply.github.com>
2024-02-28 22:22:19 -0600

    Changed image logo to  text logo for dynamic modification

--- zen-hotel-system/marriot/frontend/src/components/home/header.tsx
+++ zen-hotel-system/marriot/frontend/src/components/home/header.tsx
@@ -1,20 +1,13 @@
-import Image from "next/image";
 import Link from "next/link";
 import { HeaderNav } from "@/components/home/headerNav";
 import { SignInButton } from "@/components/home/signInButton";
 
 export function Header() {
   return (
-    <header className="h-20 border-b shadow">
-      <div className="container flex justify-between items-center h-full">
-        <Link href="/">
-          <Image
-            src={`/zen-logo.png`}
-            alt="Zen Hotels Logo"
-            width="150"
-            height="60"
-            priority
-          />
+    <header className="border-b">
+      <div className="container py-2 flex justify-between items-center">
+        <Link href="/" className="flex items-center h-[60px]">
+          <div className="text-4xl text-foreground font-satisfy">Marriot</div>
         </Link>
         <HeaderNav />
         <SignInButton />
