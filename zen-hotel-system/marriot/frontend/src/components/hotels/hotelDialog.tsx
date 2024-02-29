62751a726e86bb68f2ef4ac39c8ae4462d340dfe
Max <111196993+NqMax@users.noreply.github.com>
2024-02-28 22:26:57 -0600

    First implementation of hotel card dialog

---
+++ zen-hotel-system/marriot/frontend/src/components/hotels/hotelDialog.tsx
@@ -0,0 +1,44 @@
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { ArrowRight } from "lucide-react";
+
+export function HotelDialog() {
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button variant="link" className="w-fit p-0 mt-auto">
+          View Hotel Details
+          <ArrowRight className="ml-1 w-4 h-4" />
+        </Button>
+      </DialogTrigger>
+      <DialogContent className="">
+        <DialogHeader>
+          <DialogTitle>Edit profiles</DialogTitle>
+          <DialogDescription>
+            Make changes to your profile here. Click save when youre done.
+          </DialogDescription>
+        </DialogHeader>
+        <div className="grid gap-4 py-4">
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Input id="name" value="Pedro Duarte" className="col-span-3" />
+          </div>
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Input id="username" value="@peduarte" className="col-span-3" />
+          </div>
+        </div>
+        <DialogFooter>
+          <Button type="submit">Save changes</Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+}
