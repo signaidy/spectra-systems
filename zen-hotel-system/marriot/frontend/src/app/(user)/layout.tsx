da00103866613415f116e2e755f168efb53a6a19
Max <111196993+NqMax@users.noreply.github.com>
2024-02-28 22:23:36 -0600

    Layout changes

--- zen-hotel-system/marriot/frontend/src/app/(user)/layout.tsx
+++ zen-hotel-system/marriot/frontend/src/app/(user)/layout.tsx
@@ -1,4 +1,4 @@
-import { Nav } from "@/components/user/nav";
+import { NavBar } from "@/components/user/navBar";
 
 export default function UserLayout({
   children,
@@ -6,9 +6,9 @@ export default function UserLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="relative flex container h-[calc(100vh-5rem)] overflow-y-auto">
-      <Nav />
-      {children}
+    <div className="flex container h-[calc(100vh-4.813rem)] overflow-y-auto">
+      <NavBar />
+      <div className="flex flex-col py-8 pl-8 w-full">{children}</div>
     </div>
   );
 }
--- zen-hotel-system/marriot/frontend/src/app/page.tsx
+++ zen-hotel-system/marriot/frontend/src/app/page.tsx
@@ -1,9 +1,9 @@
 import Image from "next/image";
-import { SearchBar } from "@/components/searchBar";
+import { SearchBar } from "@/components/searchBar/searchBar";
 
 export default function Home() {
   return (
-    <section className="flex relative h-[calc(100vh-5rem)]">
+    <section className="flex relative h-[calc(100vh-4.813rem)]">
       <Image
         src="/home-bg.jpg"
         alt="Decorative Background"
