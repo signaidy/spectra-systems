eb5eca3122b127fa1992c8a362aeb02b004c3ebe
Max <111196993+NqMax@users.noreply.github.com>
2024-02-28 22:22:55 -0600

    Added error component for user routes

---
+++ zen-hotel-system/marriot/frontend/src/app/(user)/error.tsx
@@ -0,0 +1,18 @@
+"use client"; // Error components must be Client Components
+import { Button } from "@/components/ui/button";
+
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string };
+  reset: () => void;
+}) {
+  return (
+    <div className="flex flex-col gap-y-2 justify-center items-center h-full">
+      <h1 className="text-2xl font-bold">Something Went Wrong!</h1>
+      <h2 className="text-xl font-medium text-red-600">{error.message}</h2>
+      <Button onClick={() => reset()}>Try again</Button>
+    </div>
+  );
+}
