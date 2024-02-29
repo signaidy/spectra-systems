86a9d677a93d32678c2255baa82f5fcffbf5ae1d
Max <111196993+NqMax@users.noreply.github.com>
2024-02-28 22:27:10 -0600

    First implementation of search page

---
+++ zen-hotel-system/marriot/frontend/src/app/search/page.tsx
@@ -0,0 +1,29 @@
+import Image from "next/image";
+import { SearchBar } from "@/components/searchBar/searchBar";
+import { HotelCard } from "@/components/user/inventory/hotelCard";
+
+export default function SearchHome() {
+  return (
+    <section className="relative min-h-[calc(100vh-4.813rem)] bg-fixed">
+      <Image
+        src="/home-bg.jpg"
+        alt="Decorative Background"
+        quality={100}
+        fill
+        className="object-cover -z-10 fixed"
+        priority
+      />
+      <div className="container flex flex-col py-8 gap-y-10">
+        <SearchBar />
+        <HotelCard
+          name="Hotel Name"
+          description="
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident nisi eos molestiae, placeat suscipit veniam rerum aliquid, ipsa, totam officiis iusto quam tenetur incidunt omnis unde. Neque consequatur autem quae."
+          location="Hotel Location"
+          reviewCount={5}
+          reviewAverage={4.5}
+        />
+      </div>
+    </section>
+  );
+}
