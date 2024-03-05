0a1a37acc638c6ebeed1c7d23de96af67b7ae066
Max <111196993+NqMax@users.noreply.github.com>
2024-03-04 21:51:54 -0600

    First implementation of footer

--- zen-hotel-system/marriot/frontend/src/components/home/footer.tsx
+++ zen-hotel-system/marriot/frontend/src/components/home/footer.tsx
@@ -1,3 +1,9 @@
 export function Footer() {
-  return <></>;
+  return <footer className="flex container py-8 gap-x-10">
+    <div className="text-4xl text-foreground font-satisfy">Marriot</div>
+    <div className="flex flex-col">
+      <div className="text-xl font-bold">Explore More</div>
+      <div>Modify Cancel</div>
+    </div>
+  </footer>;
 }
