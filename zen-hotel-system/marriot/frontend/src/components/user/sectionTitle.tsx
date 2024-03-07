6163f793126ca4ba0ec6699ce0bedf104958988b
Max <111196993+NqMax@users.noreply.github.com>
2024-03-07 00:34:22 -0600

    Added classname prop to section title

--- zen-hotel-system/marriot/frontend/src/components/user/sectionTitle.tsx
+++ zen-hotel-system/marriot/frontend/src/components/user/sectionTitle.tsx
@@ -1,14 +1,17 @@
 import { InformativeTooltip } from "@/components/ui/informativeTooltip";
+import { cn } from "@/lib/utils";
 
 export function SectionTitle({
   title,
   description,
+  className,
 }: {
   title: string;
   description: string;
+  className?: string;
 }) {
   return (
-    <div className="flex gap-x-3">
+    <div className={cn("flex gap-x-3", className)}>
       <h1 className="font-bold text-xl">{title}</h1>
       <InformativeTooltip description={description} />
     </div>
