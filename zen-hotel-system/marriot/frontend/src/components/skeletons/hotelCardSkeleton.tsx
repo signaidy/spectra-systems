f0f83d693ddfa72892a770189d562d8b8de7f5d8
Max <111196993+NqMax@users.noreply.github.com>
2024-03-07 00:33:27 -0600

    Hotel Card Skeleton UI Changes

--- zen-hotel-system/marriot/frontend/src/components/skeletons/hotelCardSkeleton.tsx
+++ zen-hotel-system/marriot/frontend/src/components/skeletons/hotelCardSkeleton.tsx
@@ -1,21 +1,16 @@
 export function HotelCardSkeleton() {
   return (
-    <article className="border rounded-md grid-cols-[0.75fr_1fr] grid h-72 animate-pulse bg-muted">
+    <article className="border rounded-md grid-cols-[0.75fr_1fr] grid h-72 bg-muted animate-pulse">
       <div className="relative bg-muted-foreground rounded-l-md" />
-      <div className="flex flex-col justify-between rounded-r-md">
-        <div className="flex flex-col gap-y-1 p-4">
-          <div className="w-96 h-8 bg-muted-foreground rounded-md" />
-          <div className="w-80 h-5 bg-muted-foreground rounded-md" />
-          <div className="flex flex-col gap-y-6 mt-5">
-            <div className="w-full h-10 bg-muted-foreground rounded-md" />
-            <div className="w-36 h-5 bg-muted-foreground rounded-md" />
-          </div>
+      <div className="flex flex-col justify-between rounded-r-md bg-background">
+        <div className="flex flex-col gap-y-1 p-4 h-full">
+          <div className="h-7 w-80 bg-muted-foreground rounded-lg" />
+          <div className="h-5 w-72 bg-muted-foreground rounded-lg" />
+          <div className="h-10 w-full bg-muted-foreground rounded-lg mt-4" />
+          <div className="h-7 w-[142px] bg-muted-foreground rounded-lg mt-auto" />
         </div>
-        <div className="border-t p-4 flex justify-end gap-x-3">
-          <div className="flex gap-x-1 items-end">
-            <div className="h-7 w-32 bg-muted-foreground rounded-md" />
-          </div>
-          <div className="w-28 h-10 bg-muted-foreground rounded-md" />
+        <div className="border-t p-3 flex justify-end gap-x-3">
+          <div className="h-10 w-[107px] bg-muted-foreground rounded-lg" />
         </div>
       </div>
     </article>
