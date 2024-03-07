9ab40ec82cac0f46836145906fe9f6a26c7349d6
Sir_Anon <juanpablo08082002@hotmail.com>
2024-03-07 01:07:00 -0600

    Header information load to FE

--- apex-airline-system/avianca/avianca-frontend/src/lib/components/header/header.svelte
+++ apex-airline-system/avianca/avianca-frontend/src/lib/components/header/header.svelte
@@ -5,28 +5,64 @@
   import imageTab from "$lib/assets/Image-Tab.png";
 
   export let data: { user: User | null };
+
+  let headerdata = getHeaderInformation();
+
+async function getHeaderInformation() {
+  const response = await fetch("http://localhost:8080/header");
+  const headerdata = await response.json();
+  return headerdata;
+}
 </script>
 
 <link class="tabimage" rel="icon" type="image/png" href={imageTab}/> 
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Satisfy&display=swap" rel="stylesheet">
 
+{#await headerdata}
+<header class="h-20 border-b shadow-md p-4 animate-pulse">
+  <div class="flex h-full container justify-between items-center bg-gray-300 rounded mb-2">
+    <a href="/">
+      <div class="flex">
+    </div>
+    </a>
+    <nav class="flex gap-x-3 text-sm font-bold">
+  </div>
+</header>
+
+{:then datahead}
 <header class="h-20 border-b">
   <div class="flex h-full container justify-between items-center">
     <a href="/">
-      <img src={logo} alt="Avianca Logo" class="w-[202px] h-[60px]" />
+      <div class="flex">
+      <img src="https://i.ibb.co/KGJV2hW/Image-Tab-1.png" alt="Avianca Logo" class="w-[55px] h-[50px]" /><p class="satisfy-regular">{datahead.Text_Logo}</p>
+    </div>
     </a>
     <nav class="flex gap-x-3 text-sm font-bold">
-      <Button href="/aboutus" variant="ghost" class="font-bold">About Us</Button>
+      <Button href={datahead.Link_Section} variant="ghost" class="font-bold">{datahead.Section}</Button>
     </nav>
     {#if data.user}
-      <Button href="/user/dashboard" variant="ghost" class="font-bold">
+      <Button href={datahead.Link_Profile} variant="ghost" class="font-bold">
         <CircleUserRound class="mr-2" />{data.user.firstName +
           " " +
           data.user.lastName}
       </Button>
     {:else}
-      <Button href="/signin">
+      <Button href={datahead.Link_Login}>
         Join | Sign In <CircleUserRound class="ml-2" />
       </Button>
     {/if}
   </div>
 </header>
+{/await}
+
+<style>
+  .satisfy-regular {
+  font-family: "Satisfy", cursive;
+  font-weight: 400;
+  font-size: 35px;
+  margin-left: 10px;
+  font-style: normal;
+}
+</style>
