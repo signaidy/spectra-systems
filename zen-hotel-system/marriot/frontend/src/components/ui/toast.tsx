498ef9b57d9a56b1db804a1f935d4c7236a100f1
Max <111196993+NqMax@users.noreply.github.com>
2024-03-08 21:20:46 -0600

    Added toast success variant

--- zen-hotel-system/marriot/frontend/src/components/ui/toast.tsx
+++ zen-hotel-system/marriot/frontend/src/components/ui/toast.tsx
@@ -28,6 +28,7 @@ const toastVariants = cva(
     variants: {
       variant: {
         default: "border bg-background text-foreground",
+        success: "border bg-green-600 text-destructive-foreground",
         destructive:
           "destructive group border-destructive bg-destructive text-destructive-foreground",
       },
