53411f2d494f9c90ded719d47ed1c5d976615010
Max <111196993+NqMax@users.noreply.github.com>
2024-03-12 16:26:58 -0600

    Refactored hotel card props to props general object

--- zen-hotel-system/marriot/frontend/src/components/hotels/hotelCard.tsx
+++ zen-hotel-system/marriot/frontend/src/components/hotels/hotelCard.tsx
@@ -5,51 +5,39 @@ import { HotelDialog } from "@/components/hotels/hotelDialog";
 import { Button } from "@/components/ui/button";
 import { Star, MapPin } from "lucide-react";
 
-export function HotelCard({
-  name,
-  description,
-  location,
-  reviewCount,
-  reviewAverage,
-}: {
-  name: string;
-  description: string;
-  location: string;
-  reviewCount: number;
-  reviewAverage: number;
-}) {
+export function HotelCard(props: Hotel) {
   return (
     <article className="border rounded-md grid-cols-[0.75fr_1fr] grid h-72">
       <div className="relative">
         <Image
-          src="/hotel-background.jpg"
+          src={props.picture}
           sizes="(min-width: 1536px) 535px, (min-width: 1280px) 425px, (min-width: 1024px) 230px"
-          alt={`${name} Hotel Image`}
+          alt={`${props.name} Hotel Image`}
           fill
           className="object-cover rounded-l-md"
         />
       </div>
       <div className="flex flex-col justify-between rounded-r-md bg-background">
         <div className="flex flex-col gap-y-1 p-4 h-full">
-          <div className="font-medium tracking-tight text-xl">{name}</div>
+          <div className="font-medium tracking-tight text-xl">{props.name}</div>
           <div className="flex gap-x-1 text-sm items-center text-muted-foreground">
-            <div>{reviewAverage}</div>
+            <div>{props.reviews.average}</div>
             <Star className="w-4 h-4 text-black fill-yellow-500" />
-            <div>({reviewCount} reviews)</div>
+            <div>({props.reviews.count} reviews)</div>
             <div>|</div>
             <MapPin className="w-4 h-4 text-black fill-red-700" />
-            <div>{location}</div>
+            <div>{props.location.address}</div>
           </div>
-          <p className="text-sm line-clamp-3 mt-4">{description}</p>
-          <HotelDialog />
+          <p className="text-sm line-clamp-3 mt-4">{props.description}</p>
+          <HotelDialog {...props} />
         </div>
         <div className="border-t p-3 flex justify-end gap-x-3">
-          {/* <div className="flex gap-x-1 items-end">
-            <div className="font-bold text-xl">255</div>  
+          <div className="flex gap-x-1 items-end">
+            <div className="font-bold text-xl">{props.rooms.juniorSuite.price}</div>
             <div className="text-sm mb-[2px] text-muted-foreground">
               USD / Night
             </div>
-          </div> */}
+          </div>
           <Button>View Rates</Button>
         </div>
       </div>
