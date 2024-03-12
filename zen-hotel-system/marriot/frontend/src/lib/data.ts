6d7aeaf87e4cc04bee6fe9edcc1ba20a7ce1c976
Max <111196993+NqMax@users.noreply.github.com>
2024-03-12 16:20:47 -0600

    Fetch implementation

--- zen-hotel-system/marriot/frontend/src/lib/data.ts
+++ zen-hotel-system/marriot/frontend/src/lib/data.ts
@@ -2,13 +2,37 @@ const { MongoClient } = require("mongodb");
 
 const client = new MongoClient(process.env.MONGODB_URI);
 
+export async function getLocations() {
+  try {
+    const database = client.db("marriot-db");
+    const locationsCollection = database.collection("locations");
+
+    const result = locationsCollection.find();
+
+    const locations = [];
+    for await (const location of result) {
+      locations.push({ ...location, _id: location._id.toString() });
+    }
+
+    return locations;
+  } catch (e) {
+    console.log(e);
+    throw new Error("Failed to Retrieve Cities");
+  }
+}
+
 export async function getHotels() {
   try {
     const database = client.db("marriot-db");
     const hotelsCollection = database.collection("hotels");
 
-    const hotels = hotelsCollection.find().toArray();
-    
+    const result = hotelsCollection.find();
+
+    const hotels = [];
+    for await (const hotel of result) {
+      hotels.push({ ...hotel, _id: hotel._id.toString() });
+    }
+
     return hotels;
   } catch (e) {
     console.log(e);
