eceb06f8954d70861926bc83eeee6d3a4c619d41
Sir_Anon <juanpablo08082002@hotmail.com>
2024-03-16 15:35:26 -0600

    Implementation of Update Footer in FE

--- apex-airline-system/avianca/avianca-frontend/src/routes/user/administration/footer/+page.js
+++
@@ -1,7 +0,0 @@
-export async function load({ fetch }) {
-    const response = await fetch("http://localhost:8080/footer");
-    const footer = await response.json();
-    return {
-       footer,
-    };
-  }
