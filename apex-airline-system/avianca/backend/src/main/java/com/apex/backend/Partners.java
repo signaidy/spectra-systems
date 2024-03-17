01e2d9b6f158926897b60106877a0c41cb5f407a
Sir_Anon <juanpablo08082002@hotmail.com>
2024-03-16 18:05:20 -0600

    Partners - Update API

---
+++ apex-airline-system/avianca/backend/src/main/java/com/apex/backend/Partners.java
@@ -0,0 +1,32 @@
+package com.apex.backend;
+
+public class Partners {
+    String Title;
+    String Description;
+    String Partner1;
+    String L1;
+    String Partner2;
+    String L2;
+    String Partner3;
+    String L3;
+    String Partner4;
+    String L4;
+    String Partner5;
+    String L5;
+
+    public Partners(String Title, String Description, String Partner1, String L1, String Partner2, String L2, String Partner3, String L3, String Partner4, String L4,
+    String Partner5, String L5) {
+        this.Title = Title;
+        this.Description = Description;
+        this.Partner1 = Partner1;
+        this.L1 = L1;
+        this.Partner2 = Partner2;
+        this.L2 = L2;
+        this.Partner3 = Partner3;
+        this.L3 = L3;
+        this.Partner4 = Partner4;
+        this.L4 = L4;
+        this.Partner5 = Partner5;
+        this.L5 = L5; 
+    }
+}
