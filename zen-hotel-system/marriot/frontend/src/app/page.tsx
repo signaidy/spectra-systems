666c64cbd620f102e2c00f1ffd8229a2b61b83b1
Max <111196993+NqMax@users.noreply.github.com>
2024-03-16 21:34:30 -0600

    Added cities fetching

--- zen-hotel-system/marriot/frontend/src/app/page.tsx
+++ zen-hotel-system/marriot/frontend/src/app/page.tsx
@@ -1,8 +1,13 @@
 import Image from "next/image";
+// Data
+import { getCities } from "@/lib/data";
+// Components
 import { SearchBar } from "@/components/searchBar/searchBar";
 import { Footer } from "@/components/home/footer";
 
-export default function Home() {
+export default async function Home() {
+  const cities = await getCities();
+
   return (
     <>
       <section className="flex relative h-[calc(100vh-4.813rem)]">
@@ -14,7 +19,7 @@ export default function Home() {
           className="object-cover -z-10"
           priority
         />
-        <SearchBar />
+        <SearchBar locations={cities}/>
       </section>
       <Footer />
     </>
