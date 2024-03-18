b5cc4b21d784fb708d1ff4054e50f2afc1eaad9d
Sir_Anon <juanpablo08082002@hotmail.com>
2024-03-18 02:28:18 -0600

    Function add city implemented - FE

--- apex-airline-system/avianca/avianca-frontend/src/routes/user/administration/cities/+page.svelte
+++ apex-airline-system/avianca/avianca-frontend/src/routes/user/administration/cities/+page.svelte
@@ -4,24 +4,57 @@
   import CitySkeleton from "$lib/components/user/administration/cities/citieSkeleton.svelte";
   export let data;
 
+let alert_message; 
+let display = false; 
 
+  async function AddCity() {
+    const city = document.getElementById("city").value;
+    if (city === ""){
+        alert_message = "You have to enter a city first!"; 
+        display = true; 
+    } else {
+    try {
+      const response = await fetch(
+        `http://localhost:8080/create-city/${city}`,
+        {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({}),
+        }
+      );
+    } catch (error) {
+      console.error("API error:", error);
+    }
 
-  
+    display = false; 
+
+    let shouldRefresh = true;
+
+    if (shouldRefresh) {
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+      window.location.reload();
+    }
+  }
+} 
 </script>
 
 <div class="flex items-center mt-4 mb-8 gap-x-3">
   <h1 class="text-xl font-bold">Cities administration</h1>
 </div>
 <div class="flex items-center justify-center">
-  <Button>Add city</Button>
-  <input
-    type="text"
-    id="city"
-    class="ml-2 text-center bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg p-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-    placeholder="Insert the new country"
-    style="width: 700px;"
-  />
+        <input
+        required
+        type="text"
+        id="city"
+        class="mr-2 text-center bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg p-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        placeholder="Insert the new country"
+        style="width: 700px;"
+      />
+      <Button on:click={AddCity}>Add city</Button>
 </div>
+{#if display == true}
+<p class="mt-2 text-red-500 text-center dspl">{alert_message}</p>
+{/if}
 {#await data.cities}
   {#each Array(3).fill(0) as _}
     <CitySkeleton />
@@ -33,3 +66,9 @@
 {:catch error}
   <p>{error.message}</p>
 {/await}
+
+<style>
+    .dspl{
+        margin-right: 85px;
+    }
+</style>
