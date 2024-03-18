e49ab787da8552f074a4aa9fc5c043c0baf6a91e
Max <111196993+NqMax@users.noreply.github.com>
2024-03-18 00:26:41 -0600

    Added corresponding props to room components to validate and continue hotel selection flow

--- zen-hotel-system/marriot/frontend/src/components/rooms/hotelRooms.tsx
+++ zen-hotel-system/marriot/frontend/src/components/rooms/hotelRooms.tsx
@@ -3,15 +3,37 @@ import { getHotelById } from "@/lib/data";
 // Components
 import { RoomCard } from "@/components/rooms/roomCard";
 
-export async function HotelRooms({ id }: { id: string }) {
+export async function HotelRooms({
+  id,
+  searchParams,
+}: {
+  id: string;
+  searchParams: HotelSearchParams;
+}) {
   const hotel = await getHotelById(id);
 
   return (
     <>
-      <RoomCard type="juniorSuite" {...hotel.rooms.juniorSuite} />
-      <RoomCard type="standardSuite" {...hotel.rooms.standardSuite} />
-      <RoomCard type="doubleSuite" {...hotel.rooms.doubleSuite} />
-      <RoomCard type="bigSuite" {...hotel.rooms.bigSuite} />
+      <RoomCard
+        hotelId={id}
+        suite={{ ...hotel.rooms.juniorSuite, type: "juniorSuite" }}
+        searchParams={searchParams}
+      />
+      <RoomCard
+        hotelId={id}
+        suite={{ ...hotel.rooms.standardSuite, type: "standardSuite" }}
+        searchParams={searchParams}
+      />
+      <RoomCard
+        hotelId={id}
+        suite={{ ...hotel.rooms.doubleSuite, type: "doubleSuite" }}
+        searchParams={searchParams}
+      />
+      <RoomCard
+        hotelId={id}
+        suite={{ ...hotel.rooms.bigSuite, type: "bigSuite" }}
+        searchParams={searchParams}
+      />
     </>
   );
 }
