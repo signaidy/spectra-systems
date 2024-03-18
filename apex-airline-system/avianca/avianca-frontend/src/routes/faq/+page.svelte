c71871aa286c3167cfd34767b49a3ba2bd73312c
Sir_Anon <juanpablo08082002@hotmail.com>
2024-03-18 17:35:15 -0600

    UI/UX of FAQ

---
+++ apex-airline-system/avianca/avianca-frontend/src/routes/faq/+page.svelte
@@ -0,0 +1,138 @@
+<script>
+  import Footer from "$lib/components/footer/footer.svelte";
+</script>
+
+<div class="p-8">
+  <div class="bg-white p-4 rounded-lg shadow-xl mt-12">
+    <h4
+      class="text-7xl font-bold text-indigo-600 tracking-widest uppercase text-center"
+    >
+      FAQ
+    </h4>
+    <p class="text-center text-gray-600 text-sm mt-2">
+      Here are some of the frequently asked questions
+    </p>
+    <div class="space-y-12 px-2 xl:px-16 mt-12">
+      <div class="mt-4 flex">
+        <div>
+          <div class="flex items-center h-16 border-l-4 border-blue-600">
+            <span class="text-4xl text-indigo-600 px-4">Q.</span>
+          </div>
+          <div class="flex items-center h-16 border-l-4 border-gray-400">
+            <span class="text-4xl text-gray-400 px-4">A.</span>
+          </div>
+        </div>
+        <div>
+          <div class="flex items-center h-16">
+            <span class="text-lg text-indigo-600 font-bold mt-2"
+              >Can I cancel my flight?</span
+            >
+          </div>
+          <div class="flex items-center py-2">
+            <span class="text-gray-500 mt-4"
+              >To ensure a consisten flow of the process and that the flights won't be affected in the count of the persons that are part of it,
+              we don't allow you to cancel it. </span
+            >
+          </div>
+        </div>
+      </div>
+
+      <div class="mt-4 flex">
+        <div>
+          <div class="flex items-center h-16 border-l-4 border-blue-600">
+            <span class="text-4xl text-indigo-600 px-4">Q.</span>
+          </div>
+          <div class="flex items-center h-16 border-l-4 border-gray-400">
+            <span class="text-4xl text-gray-400 px-4">A.</span>
+          </div>
+        </div>
+        <div>
+          <div class="flex items-center h-16">
+            <span class="text-lg text-indigo-600 font-bold"
+              >Where I receive the notification when a flight is canceled or modified? </span
+            >
+          </div>
+          <div class="flex items-center py-2">
+            <span class="text-gray-500 mt-4"
+              >You can check the state into your profile o you can check your email where
+              you will have receive the information related to that actions</span
+            >
+          </div>
+        </div>
+      </div>
+
+      <div class="mt-4 flex">
+        <div>
+          <div class="flex items-center h-16 border-l-4 border-blue-600">
+            <span class="text-4xl text-indigo-600 px-4">Q.</span>
+          </div>
+          <div class="flex items-center h-16 border-l-4 border-gray-400">
+            <span class="text-4xl text-gray-400 px-4">A.</span>
+          </div>
+        </div>
+        <div>
+          <div class="flex items-center h-16">
+            <span class="text-lg text-indigo-600 font-bold"
+              >Can I make multiple scale trips? </span
+            >
+          </div>
+          <div class="flex items-center py-2">
+            <span class="text-gray-500 mt-4"
+              >No, for the moment we are just providing one scale at a maximum, beacuse the deficit
+              on the number of planes</span
+            >
+          </div>
+        </div>
+      </div>
+
+      <div class="mt-4 flex">
+        <div>
+          <div class="flex items-center h-16 border-l-4 border-blue-600">
+            <span class="text-4xl text-indigo-600 px-4">Q.</span>
+          </div>
+          <div class="flex items-center h-16 border-l-4 border-gray-400">
+            <span class="text-4xl text-gray-400 px-4">A.</span>
+          </div>
+        </div>
+        <div>
+          <div class="flex items-center h-16">
+            <span class="text-lg text-indigo-600 font-bold"
+              >How can I become or suggest to be an enterprise user?</span
+            >
+          </div>
+          <div class="flex items-center py-2">
+            <span class="text-gray-500 mt-4"
+              >You can send a email to systemspectracjm@gmail.com for suggesting that rol 
+              then the IT personal will check if you are a partner member and then will be 
+              available it for you</span
+            >
+          </div>
+        </div>
+      </div>
+
+      <div class="mt-4 flex">
+        <div>
+          <div class="flex items-center h-16 border-l-4 border-blue-600">
+            <span class="text-4xl text-indigo-600 px-4">Q.</span>
+          </div>
+          <div class="flex items-center h-16 border-l-4 border-gray-400">
+            <span class="text-4xl text-gray-400 px-4">A.</span>
+          </div>
+        </div>
+        <div>
+          <div class="flex items-center h-16">
+            <span class="text-lg text-indigo-600 font-bold"
+              >Can I have multiple users with the same email?</span
+            >
+          </div>
+          <div class="flex items-center py-2">
+            <span class="text-gray-500 mt-4"
+              >For security reasons, you only can have one email attached to a user</span
+            >
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<Footer />
