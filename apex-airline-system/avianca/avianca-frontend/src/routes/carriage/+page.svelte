c1cdf1f1d61817f0c3a349e0f7887a146127a020
Sir_Anon <juanpablo08082002@hotmail.com>
2024-03-18 23:51:19 -0600

    UI/UX Carriage

---
+++ apex-airline-system/avianca/avianca-frontend/src/routes/carriage/+page.svelte
@@ -0,0 +1,128 @@
+<link rel="stylesheet" href="https://demos.creative-tim.com/notus-js/assets/styles/tailwind.css">
+<link rel="stylesheet" href="https://demos.creative-tim.com/notus-js/assets/vendor/@fortawesome/fontawesome-free/css/all.min.css">
+
+<script>
+    import Footer from "$lib/components/footer/footer.svelte";
+</script>
+
+<section class="relative pt-16 bg-blueGray-50">
+<div class="container mx-auto">
+  <div class="flex flex-wrap items-center">
+    <div class="w-10/12 md:w-6/12 lg:w-4/12 px-12 md:px-4 mr-auto ml-auto -mt-78">
+      <div class="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded-lg bg-indigo-500">
+        <img alt="..." src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQzICUKWbPeJPMxFnu3EZLGNQm4yfU8MjMjOA&usqp=CAU" class="w-full align-middle rounded-t-lg">
+        <blockquote class="relative p-8 mb-4">
+          <svg preserveAspectRatio="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 583 95" class="absolute left-0 w-full block h-95-px -top-94-px">
+            <polygon points="-30,95 583,95 583,65" class="text-indigo-500 fill-current"></polygon>
+          </svg>
+          <h4 class="text-xl font-bold text-white">
+            Carry-on suitcases
+          </h4>
+          <p class="text-md font-light mt-2 text-white text-justify">
+            Carry-on suitcases, also known as cabin luggage, are your trusty companions for short trips or times when you want to avoid checked baggage fees.
+            They are designed to fit within the overhead compartments of airplanes, allowing you to keep your essential belongings with you throughout your flight.
+          </p>
+        </blockquote>
+      </div>
+    </div>
+
+    <div class="w-full md:w-6/12 px-4">
+      <div class="flex flex-wrap">
+        <div class="w-full md:w-6/12 px-4">
+          <div class="relative flex flex-col mt-4">
+            <div class="px-4 py-5 flex-auto">
+              <div class="text-blueGray-500 p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-white">
+                <i class="fas fa-sitemap"></i>
+              </div>
+              <h6 class="text-xl mb-1 font-semibold">Size</h6>
+              <p class="mb-4 text-blueGray-500 text-justify">
+                Imagine your carry-on as a VIP – it gets to travel right next to you in the cabin!  To ensure everyone has enough space, we have size restrictions. Typically, this means your suitcase should be no bigger than 22 inches x 14 inches x 9 inches (56 x 35 x 23 cm).
+                Think of it as fitting comfortably within the sizer gauge you see at the boarding gate.  Just like showing your boarding pass, your carry-on needs to meet the size requirements to join the in-flight party.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="w-full md:w-6/12 px-4">
+          <div class="relative flex flex-col min-w-0 mt-4">
+            <div class="px-4 py-5 flex-auto">
+              <div class="text-blueGray-500 p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-white">
+                <i class="fas fa-newspaper"></i>
+              </div>
+              <h6 class="text-xl mb-1 font-semibold">Weight</h6>
+              <p class="mb-4 text-blueGray-500 text-justify">
+                We all love packing for adventures, but just like a plane needs the right balance for takeoff, your carry-on weight matters.
+                Each traveler gets a specific weight allowance, usually between 7-10 kg (15-22 lbs), which includes the suitcase itself!
+                Think of it as a weight limit for your champion carry-on. Pack strategically,  leaving heavier items like shoes and extra toiletries for checked baggage (if you have it).
+                By packing light, you'll be gliding through the airport like a pro and onto your flight feeling fantastic.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+</section>
+
+<section class="relative pt-16 bg-blueGray-50">
+    <div class="container mx-auto">
+      <div class="flex flex-wrap items-center">
+        <div class="w-full md:w-6/12 px-4">
+          <div class="flex flex-wrap">
+            <div class="w-full md:w-6/12 px-4">
+              <div class="relative flex flex-col mt-4">
+                <div class="px-4 py-5 flex-auto">
+                  <div class="text-blueGray-500 p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-white">
+                    <i class="fas fa-sitemap"></i>
+                  </div>
+                  <h6 class="text-xl mb-1 font-semibold">Size</h6>
+                  <p class="mb-4 text-blueGray-500 text-justify">
+                    Imagine your carry-on as a trusty ship, ready to brave the open skies with you! To keep our cabins shipshape for everyone, we have size restrictions. Think of it like a dockyard gate –
+                    your carry-on shouldn't be any grander than 22 inches x 14 inches x 9 inches (56 x 35 x 23 cm). Just like a captain needs to steer clear of shallow waters, your carry-on needs to fit comfortably within the sizer at the boarding gate. Remember, a smooth journey starts with a well-sized vessel!
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div class="w-full md:w-6/12 px-4">
+              <div class="relative flex flex-col min-w-0 mt-4">
+                <div class="px-4 py-5 flex-auto">
+                  <div class="text-blueGray-500 p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-white">
+                    <i class="fas fa-newspaper"></i>
+                  </div>
+                  <h6 class="text-xl mb-1 font-semibold">Weight</h6>
+                  <p class="mb-4 text-blueGray-500 text-justify">
+                    We all love to bring a bounty of goodies on our travels, but just like a galleon needs the right ballast, your carry-on's weight is important! Each salty dog (or in this case, traveler) gets a specific weight allowance, usually between 7-10 kg (15-22 lbs).
+                    That includes the weight of your carry-on itself, mind you! So, pack strategically and consider stowing heavier treasures like hefty boots and overflowing toiletry bags in the hold (checked baggage, that is). By traveling light, you'll be navigating the airport
+                    with the grace of a seasoned sailor and boarding your flight feeling light and breezy.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="w-10/12 md:w-6/12 lg:w-4/12 px-12 md:px-4 mr-auto ml-auto -mt-78">
+            <div class="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded-lg bg-indigo-500">
+              <img alt="..." src="https://hips.hearstapps.com/vader-prod.s3.amazonaws.com/1680689767-3177P-wEe-L._SL500_.jpg?crop=0.756xw:0.946xh;0.114xw,0&resize=980:*" class="w-full align-middle rounded-t-lg">
+              <blockquote class="relative p-8 mb-4">
+                <svg preserveAspectRatio="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 583 95" class="absolute left-0 w-full block h-95-px -top-94-px">
+                  <polygon points="-30,95 583,95 583,65" class="text-indigo-500 fill-current"></polygon>
+                </svg>
+                <h4 class="text-xl font-bold text-white">
+                  Big carriage
+                </h4>
+                <p class="text-md font-light mt-2 text-white text-justify">
+                  Carry-on suitcases, also known as cabin luggage, are your trusty companions for short trips or times when you want to avoid checked baggage fees.
+                  They are designed to fit within the overhead compartments of airplanes, allowing you to keep your essential belongings with you throughout your flight.
+                </p>
+              </blockquote>
+            </div>
+          </div>
+</section>
+<div class=" bg-blueGray-50" >
+<div class="relative pt-16 bg-blueGray-50 items-center text-center max-w-md mx-auto mb-10">
+    <p class="mt-2 text-black dark:text-gray-300 bg font-lg  text-xl">Rules</p>
+    <p class="mt-2 text-black text-m dark:text-gray-300 bg text-base leading-loose">Excess Baggage Fees: Ahoy, Mateys, There Be Fees Ahead!<br><br>
+      If your carry-on exceeds the weight limit, you'll incur an excess baggage fee. These fees vary depending on the weight difference and your destination, but typically start around $100 per kg (or local currency equivalent).</p>
+  </div>
+  <Footer/>
+</div>
