4495097b87c0a6f2795eece63d5db0590d91d3f3
Sir_Anon <juanpablo08082002@hotmail.com>
2024-03-19 00:26:31 -0600

    UI/UX Boarding Rules

---
+++ apex-airline-system/avianca/avianca-frontend/src/routes/boardingrules/+page.svelte
@@ -0,0 +1,85 @@
+<script>
+  import Footer from "$lib/components/footer/footer.svelte";
+</script>
+
+<div class="bg-white">
+  <div class="container mx-auto py-10 px-1">
+    <div class="w-full mx-auto bg-white rounded-lg shadow-md overflow-hidden">
+      <div class="p-4 sm:p-6">
+        <div class="items-center text-center justify-between">
+          <h2 class="text-2xl font-semibold">Boarding Rules</h2>
+        </div>
+        <hr class="my-4" />
+        <div class="text-center">
+          <p class="text-m leading-relaxed">
+            At Avianca, we're committed to getting you to your destination
+            smoothly and efficiently. This guide outlines our boarding
+            procedures to ensure a safe and orderly boarding process for
+            everyone.
+          </p>
+        </div>
+        <div class="mt-6">
+          <h3 class="text-lg font-semibold mb-2">Boarding Process:</h3>
+          <div>
+            <p>
+              Check-in: Complete the check-in process online or at the airport
+              kiosk to obtain your boarding pass. This will contain your
+              boarding group and any special instructions. Boarding Time: Keep
+              an eye on the departure screens for your flight's boarding time.
+              Boarding typically begins 30-45 minutes before departure. Gate
+              Information: Proceed to your designated boarding gate when
+              boarding commences.
+            </p>
+          </div>
+        </div>
+        <div class="mt-6">
+          <h3 class="text-lg font-semibold mb-2">Before Boarding:</h3>
+          <div>
+            <p>
+              We utilize a boarding group system to prioritize passengers and
+              ensure a smooth boarding process. Boarding groups are typically
+              announced at the gate and displayed on your boarding pass.
+            </p>
+          </div>
+        </div>
+        <div class="mt-6">
+          <h3 class="text-lg font-semibold mb-2">General Boarding</h3>
+          <div>
+            <p>
+              Boarding announcements will call specific boarding groups by
+              number. Please wait at your designated waiting area until your
+              group is called. For a faster boarding process, have your boarding
+              pass readily available and any checked baggage tags detached for
+              easy scanning.
+            </p>
+          </div>
+        </div>
+        <div class="mt-6">
+          <h3 class="text-lg font-semibold mb-2">Overhead Compartments</h3>
+          <div>
+            <p>
+              To maximize space and ensure a smooth boarding process, we
+              recommend checking any larger carry-on luggage that won't fit
+              under the seat in advance. We recommend placing any smaller
+              belongings that can fit under the seat in front of you for easy
+              access during the flight.
+            </p>
+          </div>
+        </div>
+        <div class="mt-6">
+          <h3 class="text-lg font-semibold mb-2">Seat Belts</h3>
+          <div>
+            <p>
+              Once on board, find your assigned seat and stow any carry-on
+              luggage securely in the overhead compartment or under the seat in
+              front of you. Please ensure your seat belt is securely fastened
+              before takeoff and remain fastened throughout the flight until
+              instructed by the crew.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <Footer />
+</div>
