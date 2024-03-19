ae4790dd9589856f99ce2b15c0ca979aec1f48a0
Max <111196993+NqMax@users.noreply.github.com>
2024-03-18 22:45:32 -0600

    Implemented sign in page

---
+++ zen-hotel-system/marriot/frontend/src/app/signin/page.tsx
@@ -0,0 +1,23 @@
+import Image from "next/image";
+// Components
+import { SignInForm } from "@/components/signIn/signInForm";
+import { Footer } from "@/components/home/footer";
+
+export default async function SignIn() {
+  return (
+    <>
+      <section className="flex relative h-[calc(100vh-4.813rem)]">
+        <Image
+          src="/home-bg.jpg"
+          alt="Decorative Background"
+          quality={100}
+          fill
+          className="object-cover -z-10"
+          priority
+        />
+        <SignInForm />
+      </section>
+      <Footer />
+    </>
+  );
+}
