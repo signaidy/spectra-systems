68752275fd822f3d1f42b0d781851bce7eec07ae
Max <111196993+NqMax@users.noreply.github.com>
2024-03-18 22:45:23 -0600

    Replaced specific hotel name with blueprint name

--- zen-hotel-system/marriot/frontend/src/components/home/footer.tsx
+++ zen-hotel-system/marriot/frontend/src/components/home/footer.tsx
@@ -1,7 +1,7 @@
 export function Footer() {
   return (
     <footer className="flex container py-8 gap-x-10">
-      <div className="text-4xl text-foreground font-satisfy">Marriot</div>
+      <div className="text-4xl text-foreground font-satisfy">Zen</div>
       <div className="flex flex-col gap-y-2 text-sm">
         <div className="text-base font-bold">Explore More</div>
         <div>Hotels</div>
