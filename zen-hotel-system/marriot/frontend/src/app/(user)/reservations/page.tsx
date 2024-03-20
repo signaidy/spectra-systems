dbc9d77f963bd7544b298085eacb54e2bb0f04ad
Max <111196993+NqMax@users.noreply.github.com>
2024-03-20 13:27:06 -0600

    Added reservations home page

---
+++ zen-hotel-system/marriot/frontend/src/app/(user)/reservations/page.tsx
@@ -0,0 +1,12 @@
+import { SectionTitle } from "@/components/user/sectionTitle";
+
+export default function ReservationsHome() {
+  return (
+    <>
+      <SectionTitle
+        title="My Reservations"
+        description="View Your History of Past and Current Reservations"
+      />
+    </>
+  );
+}
