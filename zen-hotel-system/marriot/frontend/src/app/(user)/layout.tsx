6cb9294b61565ba3923a09b922ccfcc866942c88
Max <111196993+NqMax@users.noreply.github.com>
2024-03-20 01:18:19 -0600

    Updated user layout to render elements conditionally

--- zen-hotel-system/marriot/frontend/src/app/(user)/layout.tsx
+++ zen-hotel-system/marriot/frontend/src/app/(user)/layout.tsx
@@ -1,14 +1,38 @@
+import { cookies } from "next/headers";
+// Components
 import { NavBar } from "@/components/user/navBar";
+// JSON Web Token Utilities
+import * as jose from "jose";
 
-export default function UserLayout({
+export default async function UserLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  let isAdmin = false;
+  const token = cookies().get("token");
+
+  if (token) {
+    try {
+      const { payload }: { payload: User } = await jose.jwtVerify(
+        token.value,
+        new TextEncoder().encode(process.env.JWT_SECRET)
+      );
+
+      if (payload.role === "admin" || payload.role === "webService") {
+        isAdmin = true;
+      }
+    } catch (e) {
+      isAdmin = false;
+    }
+  }
+
   return (
     <div className="flex container h-[calc(100vh-4.813rem)] overflow-y-auto custom-scrollbar">
-      <NavBar />
-      <div className="flex flex-col pt-8 pl-8 w-full overflow-x-auto custom-scrollbar">{children}</div>
+      <NavBar isAdmin={isAdmin} />
+      <div className="flex flex-col pt-8 pl-8 w-full overflow-x-auto custom-scrollbar">
+        {children}
+      </div>
     </div>
   );
 }
