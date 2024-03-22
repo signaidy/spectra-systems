0d01e95cb40daae84a5b320451f97d920c24fa44
Sir_Anon <juanpablo08082002@hotmail.com>
2024-03-21 22:30:49 -0600

    Incorporation of hotel search bar UI/UX

---
+++ nexus-agency-system/frontend/src/lib/components/hotelsearch/reservation.svelte
@@ -0,0 +1,65 @@
+<script lang="ts">
+  import LocationPicker from "$lib/components/hotelsearch/locationPicker.svelte";
+  import LocationPickerSkeleton from "$lib/components/hotelsearch/locationPicker.svelte";
+  import DateRangePicker from "$lib/components/hotelsearch/dateRangePicker.svelte";
+  import NumberPicker from "$lib/components/hotelsearch/numberPicker.svelte";
+  import { Button } from "$lib/components/ui/button";
+  // Icons
+  import { LandPlot, PlaneLanding, UsersRound } from "lucide-svelte";
+  // Utilities
+  import type { DateRange } from "bits-ui";
+  import { goto } from "$app/navigation";
+
+  export let cities: Promise<any> = [];
+
+  let isValid = true;
+
+  let city: string;
+  let days: DateRange | undefined;
+  let passengers: number;
+
+  function searchFlights() {
+    if (city && days && days.start && days.end && passengers) {
+      const searchParams = new URLSearchParams();
+      searchParams.append("city", city);
+      searchParams.append("check-in", days.start.toString());
+      searchParams.append("check-out", days.end.toString());
+      searchParams.append("passengers", passengers.toString());
+      goto(`/search?${searchParams.toString()}`);
+    } else {
+      isValid = false;
+    }
+  }
+</script>
+
+{#if !isValid}
+  <div class="ml-3 my-px text-red-500 font-medium">Please fill in all the fields</div>
+{/if}
+<div class="bg-background rounded-lg p-3 shadow flex gap-x-3 flex-wrap gap-y-3">
+  <div class="flex flex-col gap-y-2 w-fit">
+    <div class="font-bold flex gap-x-2 items-center">
+      Cities<LandPlot class="shrink-0 w-5 h-5" />
+    </div>
+    {#await cities}
+      <LocationPickerSkeleton />
+    {:then cities}
+      <LocationPicker {cities} bind:value={city} />
+    {:catch error}
+      <p>{error.message}</p>
+    {/await}
+  </div>
+  <div class="flex flex-col gap-y-2 w-fit">
+    <div class="font-bold">Check In - Check out</div>
+    <DateRangePicker bind:value={days} />
+  </div>
+  <div class="flex flex-col gap-y-2 w-fit">
+    <div class="font-bold flex gap-x-2 items-center ml-10">
+      Guests<UsersRound class="shrink-0 w-5 h-5" />
+    </div>
+    <NumberPicker bind:passengers />
+  </div>
+  <div class="flex flex-col gap-y-2 w-fit">
+    <div class="text-background">Find</div>
+    <Button on:click={searchFlights}>Find Flights</Button>
+  </div>
+</div>
