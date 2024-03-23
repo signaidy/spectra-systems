a7b5ebf412c56bb7fe9dde10b4b2ecc73e1c49d5
Max <111196993+NqMax@users.noreply.github.com>
2024-03-23 01:07:01 -0600

    Added footer identity features

--- zen-hotel-system/frontend/src/components/home/footer.tsx
+++ zen-hotel-system/frontend/src/components/home/footer.tsx
@@ -1,11 +1,21 @@
-export function Footer() {
+import Link from "next/link";
+import Image from "next/image";
+// Data
+import { getSiteIdentity } from "@/lib/data";
+
+export async function Footer() {
+  const { name, logo, address, copyright, contactNumber } = await getSiteIdentity();
+
   return (
-    <footer className="flex container py-8 gap-x-10">
-      <div className="text-4xl text-foreground font-satisfy">Zen Systems</div>
+    <footer className="flex container py-8 gap-x-10 border-t">
+      <div className="flex items-center h-[60px] gap-x-3">
+        <Image src={logo} alt="Chain Logo" width={60} height={60} />
+        <div className="text-4xl text-foreground font-satisfy">{name}</div>
+      </div>
       <div className="flex flex-col gap-y-2 text-sm">
-        <div className="text-base font-bold">Explore More</div>
-        <div>Hotels</div>
-        <div>Legal</div>
+        <div>{address}</div>
+        <div>{copyright}</div>
+        <div>{contactNumber}</div>
       </div>
     </footer>
   );
