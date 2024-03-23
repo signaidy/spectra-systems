d03b7fffb5e0207f508daa2dcf4a7a0e542dbf64
Max <111196993+NqMax@users.noreply.github.com>
2024-03-23 01:06:46 -0600

    Implemented logo on header

--- zen-hotel-system/frontend/src/components/home/header.tsx
+++ zen-hotel-system/frontend/src/components/home/header.tsx
@@ -1,13 +1,20 @@
 import Link from "next/link";
+import Image from "next/image";
+// Data
+import { getSiteIdentity } from "@/lib/data";
+// Components
 import { HeaderNav } from "@/components/home/headerNav";
 import { SignInButton } from "@/components/home/signInButton";
 
-export function Header() {
+export async function Header() {
+  const { name, logo } = await getSiteIdentity();
+  
   return (
     <header className="border-b">
       <div className="container py-2 flex justify-between items-center">
-        <Link href="/" className="flex items-center h-[60px]">
-          <div className="text-4xl text-foreground font-satisfy">Zen Systems</div>
+        <Link href="/" className="flex items-center h-[60px] gap-x-3">
+          <Image src={logo} alt="Chain Logo" width={60} height={60} />
+          <div className="text-4xl text-foreground font-satisfy">{name}</div>
         </Link>
         <HeaderNav />
         <SignInButton />
