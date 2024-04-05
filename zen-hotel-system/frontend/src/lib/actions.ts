19d4150dbdf78d8a99bfb8d067e00e769ef2f889
Max <111196993+NqMax@users.noreply.github.com>
2024-04-05 17:54:43 -0600

    Added action to disable reservation

--- zen-hotel-system/frontend/src/lib/actions.ts
+++ zen-hotel-system/frontend/src/lib/actions.ts
@@ -698,6 +698,26 @@ export async function enableHotel(prevState: any, formData: FormData) {
 
   revalidatePath("/");
 }
+export async function disableReservation(prevState: any, formData: FormData) {
+  try {
+    const rawFormData = Object.fromEntries(formData.entries());
+
+    const database = client.db(process.env.DB_NAME);
+    const reservations = database.collection("reservations");
+
+    await reservations.updateOne(
+      { _id: new ObjectId(rawFormData.reservationId as string) },
+      { $set: { state: "manuallyDisabled" } }
+    );
+  } catch (e) {
+    console.log(e);
+    return {
+      error: "Database Error: Failed to Disable Reservation.",
+    };
+  }
+
+  revalidatePath("/");
+}
 
 export async function logOut() {
   cookies().delete("token");
