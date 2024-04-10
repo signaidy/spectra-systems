54e4787348e0bf44c0268267c260ccaf3398a451
Max <111196993+NqMax@users.noreply.github.com>
2024-04-09 22:49:38 -0600

    Added skeleton to analytics home

--- zen-hotel-system/frontend/src/app/(user)/analytics/page.tsx
+++ zen-hotel-system/frontend/src/app/(user)/analytics/page.tsx
@@ -2,6 +2,7 @@ import { Suspense } from "react";
 // Components
 import { SectionTitle } from "@/components/user/sectionTitle";
 import { Analytics } from "@/components/user/analytics/analytics";
+import { AnalyticCardSkeleton } from "@/components/skeletons/analyticCardSkeleton";
 
 export default function AnalyticsHome() {
   return (
@@ -13,16 +14,15 @@ export default function AnalyticsHome() {
       <Suspense
         fallback={
           <div className="flex flex-col gap-y-3 mb-8 mr-8">
-            {Array(2)
+            {Array(5)
               .fill(0)
               .map((_, index) => (
-                // <ReservationCardSkeleton key={index} />
-                <div key={index}></div>
+                <AnalyticCardSkeleton key={index} />
               ))}
           </div>
         }
       >
-        <div className="flex flex-col gap-y-3 mb-8 mr-8">
+        <div className="flex gap-y-3 gap-x-3 mb-8 flex-wrap mr-8">
           <Analytics />
         </div>
       </Suspense>
