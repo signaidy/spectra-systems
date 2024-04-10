3b601a44a98a71981a69e9774341134c41812431
Max <111196993+NqMax@users.noreply.github.com>
2024-04-09 22:24:12 -0600

    Implemented analytics home component

--- zen-hotel-system/frontend/src/app/(user)/analytics/page.tsx
+++ zen-hotel-system/frontend/src/app/(user)/analytics/page.tsx
@@ -1,4 +1,7 @@
+import { Suspense } from "react";
+// Components
 import { SectionTitle } from "@/components/user/sectionTitle";
+import { Analytics } from "@/components/user/analytics/analytics";
 
 export default function AnalyticsHome() {
   return (
@@ -7,6 +10,22 @@ export default function AnalyticsHome() {
         title="Analytics"
         description="View Useful Analytics and Statistics"
       />
+      <Suspense
+        fallback={
+          <div className="flex flex-col gap-y-3 mb-8 mr-8">
+            {Array(2)
+              .fill(0)
+              .map((_, index) => (
+                // <ReservationCardSkeleton key={index} />
+                <div key={index}></div>
+              ))}
+          </div>
+        }
+      >
+        <div className="flex flex-col gap-y-3 mb-8 mr-8">
+          <Analytics />
+        </div>
+      </Suspense>
     </>
   );
 }
