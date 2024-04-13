8b5dfaefbba5534a4ac6c90e687c82cb40b447cd
Max <111196993+NqMax@users.noreply.github.com>
2024-04-13 00:28:32 -0600

    Cookies fix

--- apex-airline-system/frontend/src/routes/signin/+page.server.ts
+++ apex-airline-system/frontend/src/routes/signin/+page.server.ts
@@ -2,14 +2,14 @@ import { error, redirect } from "@sveltejs/kit";
 import { fail } from "@sveltejs/kit";
 import { JWT_SECRET } from "$env/static/private";
 import jsonwebtoken from "jsonwebtoken";
-import { PUBLIC_BASE_URL } from '$env/static/public';
+import { PUBLIC_BASE_URL } from "$env/static/public";
 
 const { sign } = jsonwebtoken;
 
 export const actions = {
   default: async ({ cookies, request }) => {
     const data = await request.formData();
-    const captcha = data.get("g-recaptcha-response"); 
+    const captcha = data.get("g-recaptcha-response");
 
     if (data.get("password") !== data.get("confirmedPassword")) {
       return fail(400, {
@@ -17,29 +17,31 @@ export const actions = {
       });
     }
 
-    if (data.get("g-recaptcha-response") == '')
-    {
+    if (data.get("g-recaptcha-response") == "") {
       return fail(400, {
         error: "Fill up captcha verification",
       });
     }
-    
+
     try {
-      const response = await fetch(`${PUBLIC_BASE_URL}/create-user/${captcha}`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          email: data.get("email"),
-          password: data.get("password"),
-          firstName: data.get("firstName"),
-          lastName: data.get("lastName"),
-          originCountry: data.get("originCountry"),
-          passportNumber: data.get("passportNumber"),
-          age: data.get("age"),
-        }),
-      });
+      const response = await fetch(
+        `${PUBLIC_BASE_URL}/create-user/${captcha}`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            email: data.get("email"),
+            password: data.get("password"),
+            firstName: data.get("firstName"),
+            lastName: data.get("lastName"),
+            originCountry: data.get("originCountry"),
+            passportNumber: data.get("passportNumber"),
+            age: data.get("age"),
+          }),
+        }
+      );
       const result = await response.json();
 
       if (result.error) {
@@ -48,7 +50,7 @@ export const actions = {
 
       const token = sign(result, JWT_SECRET);
 
-      cookies.set("token", token, { path: "/" });
+      cookies.set("token", token, { path: "/", secure: false });
     } catch (error) {
       if (error instanceof Error) {
         return fail(500, {
