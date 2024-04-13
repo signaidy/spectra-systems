94a301b490516ccebbca51dace778b12b4696a95
signaidy <cesolares@gmail.com>
2024-04-13 09:26:15 -0600

    comment on seedData to not break stuff

--- nexus-agency-system/backend/src/main/java/SpectraSystems/Nexus/config/SeedDataConfig.java
+++ nexus-agency-system/backend/src/main/java/SpectraSystems/Nexus/config/SeedDataConfig.java
@@ -46,27 +46,27 @@ public class SeedDataConfig implements CommandLineRunner {
         userService.save(admin);
         log.debug("created ADMIN user - {}", admin);
       }
-      if (aboutusRespository.count() == 0) {
+      // if (aboutusRespository.count() == 0) {
 
-        Aboutus admin = Aboutus
-                      .builder()
-                      .slogan("UwU")
-                      .gif("https://i.pinimg.com/originals/31/a7/2a/31a72afda250825d993400c3ef28c55c.gif")
-                      .yt("https://www.youtube.com/embed/a5plqZLfxRE?si=pPvS-7aP8J0ek8HY")
-                      .cardsAmount(2)
-                      .titleOne("ez")
-                      .imgOne("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_qnBaqCekVMgJ6kDf68fu_dv8znl_3czptk3F-NWnqw&s")
-                      .titleTwo("gateto")
-                      .imgTwo("https://i.imgflip.com/2/4mol3v.jpg")
-                      .titleThree(null)
-                      .imgThree(null)
-                      .titleFour(null)
-                      .imgFour(null)
-                      .build();
+      //   Aboutus admin = Aboutus
+      //                 .builder()
+      //                 .slogan("UwU")
+      //                 .gif("https://i.pinimg.com/originals/31/a7/2a/31a72afda250825d993400c3ef28c55c.gif")
+      //                 .yt("https://www.youtube.com/embed/a5plqZLfxRE?si=pPvS-7aP8J0ek8HY")
+      //                 .cardsAmount(2)
+      //                 .titleOne("ez")
+      //                 .imgOne("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_qnBaqCekVMgJ6kDf68fu_dv8znl_3czptk3F-NWnqw&s")
+      //                 .titleTwo("gateto")
+      //                 .imgTwo("https://i.imgflip.com/2/4mol3v.jpg")
+      //                 .titleThree(null)
+      //                 .imgThree(null)
+      //                 .titleFour(null)
+      //                 .imgFour(null)
+      //                 .build();
 
-        aboutUsService.saveOrUpdate(admin);
-        log.debug("created AboutUs info - {}", admin);
-      }
+      //   aboutUsService.saveOrUpdate(admin);
+      //   log.debug("created AboutUs info - {}", admin);
+      // }
     }
 
 }
