32fe101fbe789e07e9cdecf2e6e503720fbc6ef6
Max <111196993+NqMax@users.noreply.github.com>
2024-04-13 01:21:43 -0600

    Added stats

--- zen-hotel-system/frontend/src/components/user/analytics/analytics.tsx
+++ zen-hotel-system/frontend/src/components/user/analytics/analytics.tsx
@@ -1,5 +1,6 @@
 // Data
 import { getAnalytics } from "@/lib/data";
+import { getStats } from "@/lib/data";
 // Components
 import { AnalyticCard } from "@/components/user/analytics/analyticCard";
 
@@ -15,12 +16,30 @@ export async function Analytics({
   };
 }) {
   const analytics = await getAnalytics(searchParams);
+  const stats = await getStats();
 
   return (
     <>
       {analytics.length === 0 && (
-        <div className="flex justify-center items-center h-full">
+        <div
+          className={`flex justify-center items-center ${
+            stats.length > 0 ? "" : "h-full"
+          }`}
+        >
           <h1 className="text-xl font-bold">No Analytics Found</h1>
+        </div>
+      )}
+      {stats && (
+        <div className="flex flex-col">
+          <div className="font-bold">Statistics</div>
+          <div className="flex flex-wrap gap-y-2 gap-x-2">
+            {stats.map((stat, index) => (
+              <div key={index} className="pr-2 border-r">
+                {stat._id.location} searched {stat.count} times via{" "}
+                {stat._id.source}
+              </div>
+            ))}
+          </div>
         </div>
       )}
       <div className="flex gap-y-3 flex-wrap gap-x-4">
