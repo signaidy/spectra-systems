86f5aefe4b14d4b942b494a17456b79c22c21173
jp1593 <juanpablo08082002@hotmail.com>
2024-04-27 20:43:42 -0600

    JavaDoc comment - Home

--- apex-airline-system/backend/src/main/java/com/apex/backend/Home.java
+++ apex-airline-system/backend/src/main/java/com/apex/backend/Home.java
@@ -1,5 +1,15 @@
 package com.apex.backend;
 
+/**
+ * La clase `Home` representa la información de la página principal.
+
+ * Esta clase almacena las rutas o referencias a las imágenes de fondo y de vuelos 
+ * destacados, junto con los títulos y contenidos asociados a cada sección destacada 
+ * en la página principal.
+
+  * @author Juan Pablo Estrada Lucero
+  * @version 1.0
+  */
 
 public class Home {
     String Background;
@@ -13,7 +23,20 @@ public class Home {
     String Title3;
     String Content3;
 
-   
+    /**
+     * Constructor de la clase `Home`.
+     * 
+     * @param Background Ruta de la imagen de fondo de la página principal.
+     * @param FlightImage1 Ruta o referencia a la imagen del primer vuelo destacado.
+     * @param Title1 Título del primer vuelo destacado.
+     * @param Content1 Contenido descriptivo del primer vuelo destacado.
+     * @param FlightImage2 Ruta o referencia a la imagen del segundo vuelo destacado.
+     * @param Title2 Título del segundo vuelo destacado.
+     * @param Content2 Contenido descriptivo del segundo vuelo destacado.
+     * @param FlightImage3 Ruta o referencia a la imagen del tercer vuelo destacado.
+     * @param Title3 Título del tercer vuelo destacado.
+     * @param Content3 Contenido descriptivo del tercer vuelo destacado.
+     */
 
   public Home(String Background,
   String FlightImage1,
