4dde65cadbba8173b1c228b4c9a32a5f319d5f68
jp1593 <juanpablo08082002@hotmail.com>
2024-04-27 21:30:12 -0600

    JavaDoc comment - Ticket

--- apex-airline-system/backend/src/main/java/com/apex/backend/Ticket.java
+++ apex-airline-system/backend/src/main/java/com/apex/backend/Ticket.java
@@ -1,13 +1,46 @@
 package com.apex.backend;
 
+/**
+ * La clase `Ticket` representa un boleto de vuelo.
 
+ * Esta clase almacena la información básica de un boleto, incluyendo el identificador único 
+ * del boleto, su precio, el identificador del vuelo asociado, el tipo de boleto 
+ * y el estado del boleto.
+
+  * @author Juan Pablo Estrada Lucero
+  * @version 1.0
+  */
 public class Ticket {
+    /**
+     * Identificador único del boleto.
+     */
     Integer ticket_id;
+    /**
+     * Precio del boleto.
+     */
     Integer price;
+    /**
+     * Identificador del vuelo asociado al boleto.
+     */
     Integer flight_id; 
+     /**
+     * Tipo de boleto
+     */
     Integer type;
+    /**
+     * Estado de boleto
+     */
     Integer state;
 
+    /**
+     * Constructor de la clase `Ticket`.
+     * 
+     * @param ticket_id Identificador único del boleto.
+     * @param price Precio del boleto.
+     * @param flight_id Identificador del vuelo asociado al boleto.
+     * @param type Tipo de boleto
+     * @param state Estado del boleto
+     */
     public Ticket(Integer ticket_id, Integer price, Integer flight_id, Integer type, Integer state) {
         this.ticket_id = ticket_id; 
         this.price = price; 
