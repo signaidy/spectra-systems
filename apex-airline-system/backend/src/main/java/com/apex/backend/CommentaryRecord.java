119e47cde4f5065ba5a54a49839e3e559a37b79f
jp1593 <juanpablo08082002@hotmail.com>
2024-04-27 20:14:00 -0600

    JavaDoc comment - CommentaryRecord

--- apex-airline-system/backend/src/main/java/com/apex/backend/CommentaryRecord.java
+++ apex-airline-system/backend/src/main/java/com/apex/backend/CommentaryRecord.java
@@ -1,7 +1,16 @@
 package com.apex.backend;
 
 import java.util.List;
+/**
+ * La clase `CommentaryRecord` es un registro que representa un comentario asociado a un vuelo, 
+ * incluyendo información adicional.
 
+ * Esta clase permite tener un record del commentario al momento de utilizarlo en algunas APIS tiene la misma, 
+ * funcionalidad que la clase `Commentary`
+
+  * @author Juan Pablo Estrada Lucero
+  * @version 1.0
+  */
 public record CommentaryRecord(int commentId, int parentCommentId, int userId, String content, String creationDate,
         String path, int flightId, String userName, List<CommentaryRecord> children) {
 
