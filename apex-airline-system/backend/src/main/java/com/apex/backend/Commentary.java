6c2e26d0f39593eca3983d0422b939ff641b25f0
jp1593 <juanpablo08082002@hotmail.com>
2024-04-27 20:12:25 -0600

    JavaDoc comment - Commentary

--- apex-airline-system/backend/src/main/java/com/apex/backend/Commentary.java
+++ apex-airline-system/backend/src/main/java/com/apex/backend/Commentary.java
@@ -1,11 +1,50 @@
 package com.apex.backend;
 
+/**
+ * La clase `Commentary` representa un comentario asociado a un vuelo.
+
+  * Esta clase almacena la información de un comentario realizado por un usuario 
+  * sobre un vuelo específico.
+
+  * @author Juan Pablo Estrada Lucero
+  * @version 1.0
+  */
 public class Commentary {
+    /**
+     * Identificador del comentario padre (opcional).
+     * 
+     * Este atributo se utiliza para crear relacion de comentarions, donde un comentario
+     * puede responder a otro comentario existente. Si el comentario no es una respuesta
+     * a otro, este valor será 0.
+     */
     int parentId;
+     /**
+     * Identificador del usuario que realizó el comentario.
+     * 
+     * Este atributo permite vincular el comentario a un usuario específico del sistema.
+     */
     int userId;
+    /**
+     * Contenido del comentario.
+     * 
+     * Este atributo representa el texto que el usuario escribió en el comentario.
+     */
     String content;
+     /**
+     * Identificador del vuelo al que está asociado el comentario.
+     * 
+     * Este atributo vincula el comentario a un vuelo específico del sistema.
+     */
     int flightId;
 
+    /**
+     * Constructor de la clase `Commentary`.
+     * 
+     * @param parentId Identificador del comentario padre (opcional).
+     * @param userId Identificador del usuario que realizó el comentario.
+     * @param content Contenido del comentario.
+     * @param flightId Identificador del vuelo al que está asociado el comentario.
+     */
     public Commentary(
             int parentId,
             int userId,
