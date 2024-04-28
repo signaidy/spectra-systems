1dbcdb9e681e22c9cf973d0316fb657e74b986df
jp1593 <juanpablo08082002@hotmail.com>
2024-04-27 20:38:48 -0600

    JavaDoc comment - Footer

--- apex-airline-system/backend/src/main/java/com/apex/backend/Footer.java
+++ apex-airline-system/backend/src/main/java/com/apex/backend/Footer.java
@@ -1,5 +1,14 @@
 package com.apex.backend;
 
+/**
+ * La clase `Footer` representa la información del pie de página de sitio.
+
+ * Esta clase almacena los textos y enlaces que se muestran en el pie de página 
+ * agrupados en secciones para una mejor organización.
+
+  * @author Juan Pablo Estrada Lucero
+  * @version 1.0 
+  */
 public class Footer {
     String Title_1;
     String Section_1;
@@ -27,6 +36,32 @@ public class Footer {
 
     String copyright; 
 
+     /**
+     * Constructor de la clase `Footer`.
+     * 
+     * @param Title_1 Título principal de la primera sección del pie de página.
+     * @param Section_1 Título de la primera subsección de la primera sección.
+     * @param L1 Link de la primera seccion.
+     * @param Section_2 Título de la segunda subsección de la primera sección.
+     * @param L2 Link de la segunda seccion.
+     * @param Section_3 Título de la tercera subsección de la primera sección.
+     * @param L3 Link de la tercera seccion.
+     * @param Section_4 Título de la cuarta subsección de la primera sección.
+     * @param L4 Link de la cuarta seccion.
+     * @param Section_5 Título de la quinta subsección de la primera sección.
+     * @param L5 Link de la quinta seccion.
+     * @param Section_6 Título de la sexta subsección de la primera sección.
+     * @param L6 Link de la sexta seccion.
+     * @param Title_2 Título principal de la segunda sección del pie de página.
+     * @param  Quick_Section_1 Titulo de la primera subseccion de la segunda seccion
+     * @param  Link_quick_1 Link de la primera subseccion de la segunda seccion
+     * @param  Quick_Section_2 Titulo de la segunda subseccion de la segunda seccion
+     * @param  Link_quick_2 Link de la segunda subseccion de la segunda seccion
+     * @param Title_3 Título principal de la tercera sección del pie de página.
+     * @param Contact_1 Informacion de contacto 1.
+     * @param Contact_1 Informacion de contacto 2.
+     * @param copyright Texto de copyright del pie de página.
+     */
 
     public Footer(    String Title_1,
     String Section_1,
