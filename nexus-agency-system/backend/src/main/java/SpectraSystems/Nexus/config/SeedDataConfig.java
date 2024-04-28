11f63175124b95987c1a30ea642ef4d8dcc1958c
signaidy <cesolares@gmail.com>
2024-04-28 17:24:15 -0600

    seed data for aboutus

--- nexus-agency-system/backend/src/main/java/SpectraSystems/Nexus/config/SeedDataConfig.java
+++ nexus-agency-system/backend/src/main/java/SpectraSystems/Nexus/config/SeedDataConfig.java
@@ -51,31 +51,31 @@ public class SeedDataConfig implements CommandLineRunner {
         userService.save(admin);
         log.debug("created ADMIN user - {}", admin);
       }
-      // if (aboutusRespository.count() == 0) {
+      if (aboutusRespository.count() == 0) {
 
-      //   Aboutus admin = Aboutus
-      //                 .builder()
-      //                 .slogan("UwU")
-      //                 .gif("https://i.pinimg.com/originals/31/a7/2a/31a72afda250825d993400c3ef28c55c.gif")
-      //                 .yt("https://www.youtube.com/embed/a5plqZLfxRE?si=pPvS-7aP8J0ek8HY")
-      //                 .cardsAmount(2)
-      //                 .titleOne("ez")
-      //                 .textOne("xdxdxd")
-      //                 .imgOne("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_qnBaqCekVMgJ6kDf68fu_dv8znl_3czptk3F-NWnqw&s")
-      //                 .titleTwo("gateto")
-      //                 .textTwo("xdxdxd")
-      //                 .imgTwo("https://i.imgflip.com/2/4mol3v.jpg")
-      //                 .titleThree(null)
-      //                 .textThree(null)
-      //                 .imgThree(null)
-      //                 .titleFour(null)
-      //                 .textFour(null)
-      //                 .imgFour(null)
-      //                 .build();
+        Aboutus admin = Aboutus
+                      .builder()
+                      .slogan("UwU")
+                      .gif("https://i.pinimg.com/originals/31/a7/2a/31a72afda250825d993400c3ef28c55c.gif")
+                      .yt("https://www.youtube.com/embed/a5plqZLfxRE?si=pPvS-7aP8J0ek8HY")
+                      .cardsAmount(2)
+                      .titleOne("ez")
+                      .textOne("xdxdxd")
+                      .imgOne("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_qnBaqCekVMgJ6kDf68fu_dv8znl_3czptk3F-NWnqw&s")
+                      .titleTwo("gateto")
+                      .textTwo("xdxdxd")
+                      .imgTwo("https://i.imgflip.com/2/4mol3v.jpg")
+                      .titleThree(null)
+                      .textThree(null)
+                      .imgThree(null)
+                      .titleFour(null)
+                      .textFour(null)
+                      .imgFour(null)
+                      .build();
 
-      //   aboutUsService.saveOrUpdate(admin);
-      //   log.debug("created AboutUs info - {}", admin);
-      // }
+        aboutUsService.saveOrUpdate(admin);
+        log.debug("created AboutUs info - {}", admin);
+      }
     }
 
 }
