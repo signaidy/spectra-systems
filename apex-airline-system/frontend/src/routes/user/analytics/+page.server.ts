27e6cf84593943ef0e132615f01cf669fa2a9807
jp1593 <juanpablo08082002@hotmail.com>
2024-05-01 19:52:50 -0600

    Graphics Component creation and call in Analytics

--- apex-airline-system/frontend/src/routes/user/analytics/+page.server.ts
+++ apex-airline-system/frontend/src/routes/user/analytics/+page.server.ts
@@ -1,40 +1,6 @@
-import { PUBLIC_BASE_URL } from '$env/static/public';
-
 export function load({ locals }) {
-  async function datagraph2() {
-    const response = await fetch(`${PUBLIC_BASE_URL}/citysearchgraph`, {
-      method: "GET",
-    });
-
-    const result = await response.json();
-    return result;
-  }
-
-  async function datagraph1() {
-    const response = await fetch(`${PUBLIC_BASE_URL}/typesearch`, {
-      method: "GET",
-    });
-
-    const result = await response.json();
-    return result;
-  }
-
-  async function datagraph3() {
-    const response = await fetch(`${PUBLIC_BASE_URL}/userspurchasedata`, {
-      method: "GET",
-    });
-
-    const result = await response.json();
-    return result;
-  }
-
-
-
   return {
     user: locals.user,
-    G1: datagraph1(), 
-    G2: datagraph2(),
-    G3: datagraph3()
   };
 }
 
