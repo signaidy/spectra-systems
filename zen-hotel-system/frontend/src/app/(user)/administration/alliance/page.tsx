213de52d03d9f40938b3410ee82aff92a7f94788
Max <111196993+NqMax@users.noreply.github.com>
2024-05-03 14:56:29 -0600

    Added alliance administration home

---
+++ zen-hotel-system/frontend/src/app/(user)/administration/alliance/page.tsx
@@ -0,0 +1,35 @@
+import { Suspense } from "react";
+// Components
+import { SectionTitle } from "@/components/user/sectionTitle";
+import { Agencies } from "@/components/agencies/agencies";
+import { AllianceCreationForm } from "@/components/alliance/allianceCreationForm";
+
+export default function AllianceAdministrationHome() {
+  return (
+    <>
+      <SectionTitle
+        title="Airline Alliance Administration"
+        description="Manage and Configure Airline Alliance"
+      />
+      <AllianceCreationForm />
+      <Suspense
+        fallback={
+          <div className="flex flex-col gap-y-3 mt-5 mb-8 mr-8 flex-wrap">
+            {Array(5)
+              .fill(0)
+              .map((_, index) => (
+                <div
+                  key={index}
+                  className="h-28 rounded-lg bg-muted animate-pulse w-56"
+                />
+              ))}
+          </div>
+        }
+      >
+        <div className="flex gap-x-3 gap-y-3 mt-5 mb-8 mr-8 flex-wrap">
+          <Agencies />
+        </div>
+      </Suspense>
+    </>
+  );
+}
