ea998f967891c3247aa37966f275b29438910b56
Max <111196993+NqMax@users.noreply.github.com>
2024-08-28 23:33:03 -0600

    Svelte

--- apex-airline-system/frontend/src/routes/+page.svelte
+++ apex-airline-system/frontend/src/routes/+page.svelte
@@ -1,9 +1,18 @@
 <div
-  class="h-[calc(100vh-5rem)] bg-cover"
-  style="background-image: url('$lib/assets/home-background.jpg');"
+  class="h-[calc(100vh)] bg-cover"
+  style="background-image: url('/home-background.jpg');"
 >
-  <div class="flex flex-col container justify-center items-center h-full">
-    <div class="text-5xl font-bold underline bg-white p-3 rounded-lg">
+  <div class="flex w-full items-center gap-x-5 p-2 h-12 bg-black">
+    <div class="text-white font-bold">Spectra Systems △</div>
+    <div class="text-white font-bold">Flights △</div>
+    <div class="text-white font-bold">News △</div>
+    <div class="text-white font-bold">Offers △</div>
+    <div class="text-white font-bold">Partners △</div>
+  </div>
+  <div
+    class="flex flex-col container gap-y-10 justify-center items-center"
+  >
+    <div class="text-5xl font-bold bg-white p-3 rounded-lg mt-20">
       Apex Airline System
     </div>
     <div class="text-xl bg-white p-3 rounded-lg">No flights available.</div>
