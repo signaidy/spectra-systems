37c471a6981439579fdaeea43bc7aa182ddab4fc
signaidy <cesolares@gmail.com>
2024-08-29 00:47:41 -0600

    asd

--- nexus-agency-system/frontend/src/routes/+page.server.ts
+++ nexus-agency-system/frontend/src/routes/+page.server.ts
@@ -1,27 +1,27 @@
 import { PUBLIC_BACKEND_URL } from '$env/static/public';
 
 export function load({ locals }) {
-  async function getCities() {
-    const response = await fetch(`${PUBLIC_BACKEND_URL}/flights/avianca/cities`, {
-      method: "GET",
-    });
+  // async function getCities() {
+  //   const response = await fetch(`${PUBLIC_BACKEND_URL}/flights/avianca/cities`, {
+  //     method: "GET",
+  //   });
 
-    const result = await response.json();
-    return result;
-  }
+  //   const result = await response.json();
+  //   return result;
+  // }
 
-  async function getCitiesHotels() {
-    const response = await fetch(`${PUBLIC_BACKEND_URL}/reservations/cities`, {
-      method: "GET",
-    });
+  // async function getCitiesHotels() {
+  //   const response = await fetch(`${PUBLIC_BACKEND_URL}/reservations/cities`, {
+  //     method: "GET",
+  //   });
 
-    const result = await response.json();
-    return result;
-  }
+  //   const result = await response.json();
+  //   return result;
+  // }
 
   return {
     user: locals.user,
-    cities: getCities(),
-    citieshotels: getCitiesHotels()
+    cities: ["Rome", "England"],
+    citieshotels: ["Rome", "England"]
   };
 }
